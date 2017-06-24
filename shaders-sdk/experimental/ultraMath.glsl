f908252e57e1ef594ae954495986181c3ac53061
CapitalKnew <capitalknew@users.noreply.github.com>
2017-06-25 01:31:50 +0800

    working on library (framework)

--- shaders-sdk/experimental/ultraMath.glsl
+++ shaders-sdk/experimental/ultraMath.glsl
@@ -16,19 +16,23 @@ float swiz(in vec4 vc){
 }
 
 float x(inout float mem){
-    return readInvocationARB((gl_SubGroupInvocationARB / 4) * 4);
+    return readInvocationARB(mem, (gl_SubGroupInvocationARB / 4) * 4);
 }
 
 float y(inout float mem){
-    return readInvocationARB((gl_SubGroupInvocationARB / 4) * 4 + 1);
+    return readInvocationARB(mem, (gl_SubGroupInvocationARB / 4) * 4 + 1);
 }
 
 float z(inout float mem){
-    return readInvocationARB((gl_SubGroupInvocationARB / 4) * 4 + 2);
+    return readInvocationARB(mem, (gl_SubGroupInvocationARB / 4) * 4 + 2);
 }
 
 float w(inout float mem){
-    return readInvocationARB((gl_SubGroupInvocationARB / 4) * 4 + 3);
+    return readInvocationARB(mem, (gl_SubGroupInvocationARB / 4) * 4 + 3);
+}
+
+float lane(inout float mem, in int l){
+    return readInvocationARB(mem, (gl_SubGroupInvocationARB / 4) * 4 + (l % 4));
 }
 
 // compact vector
@@ -43,4 +47,22 @@ float mult4(in vec4 vec, in mat4 mat){
 
 float mult4(inout mat4 mat, in vec4 vec){
     return dot( vec4(swiz(mat[0]), swiz(mat[1]), swiz(mat[2]), swiz(mat[3])), vec );
+}
+
+// is work lane (for most operations)
+bool mt(){
+    return (gl_SubGroupInvocationARB % 4) == 0;
+}
+
+
+// cross lane "cross product"
+float cross(inout float a, inout float b){
+    const uint ln = gl_SubGroupInvocationARB % 3;
+    return dot(vec2(
+        lane(a, (ln + 1) % 3),
+        lane(b, (ln + 1) % 3)
+    ), -vec2(
+        lane(b, (ln + 2) % 3),
+        lane(a, (ln + 2) % 3)
+    ));
 }
