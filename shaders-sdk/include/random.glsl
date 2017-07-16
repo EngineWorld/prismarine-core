1da6ff7bac4c8bf51bf2fc77b64af74c6454fe0a
CapitalKnew <capitalknew@users.noreply.github.com>
2017-07-16 15:05:20 +0800

    fix random

--- shaders-sdk/include/random.glsl
+++ shaders-sdk/include/random.glsl
@@ -1,7 +1,7 @@
 #ifndef _RANDOM_H
 #define _RANDOM_H
 
-uint randomClocks = 0u;
+uint randomClocks = 1u;
 uint globalInvocationSMP = 0;
 
 uint hash( uint x ) {
@@ -34,8 +34,8 @@ float random() {
 #ifdef USE_ARB_CLOCK
     return random(uvec3( globalInvocationSMP, clock2x32ARB()));
 #else
-    uint hs = randomClocks++;
-    //uint hs = randomClocks; randomClocks = hash(randomClocks);
+    //uint hs = randomClocks++;
+    uint hs = randomClocks; randomClocks = hash(randomClocks);
     //uint hs = hash(randomClocks); randomClocks = hs;
     return random(uvec3( globalInvocationSMP, RAY_BLOCK randomUniform.time, hs ));
 #endif
