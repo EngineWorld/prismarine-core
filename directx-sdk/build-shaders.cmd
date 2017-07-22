13f56ab65ac28bbdcb17e45d103062fd9615dd42
CapitalKnew <capitalknew@users.noreply.github.com>
2017-07-22 20:04:35 +0800

    ray system for HLSL

--- directx-sdk/build-shaders.cmd
+++ directx-sdk/build-shaders.cmd
@@ -1,10 +1,10 @@
 set FLAGS= -E main -T cs_5_0 -HV 2017 -WX -O0 
- dxc %FLAGS% intersection.hlsl       -Fo intersection.bin
- dxc %FLAGS% hlbvh/aabbmaker.hlsl    -Fo hlbvh/aabbmaker.bin
- dxc %FLAGS% hlbvh/build.hlsl        -Fo hlbvh/build.bin
- dxc %FLAGS% hlbvh/minmax.hlsl       -Fo hlbvh/minmax.bin
- dxc %FLAGS% hlbvh/refit.hlsl        -Fo hlbvh/refit.bin
- dxc %FLAGS% radix/histogram.hlsl    -Fo radix/histogram.bin
- dxc %FLAGS% radix/permute.hlsl      -Fo radix/permute.bin
- dxc %FLAGS% radix/prefix-scan.hlsl  -Fo radix/prefix-scan.bin
+ dxc %FLAGS% render/intersection.hlsl  -Fo render/intersection.bin
+ dxc %FLAGS% hlbvh/aabbmaker.hlsl      -Fo hlbvh/aabbmaker.bin
+ dxc %FLAGS% hlbvh/build.hlsl          -Fo hlbvh/build.bin
+ dxc %FLAGS% hlbvh/minmax.hlsl         -Fo hlbvh/minmax.bin
+ dxc %FLAGS% hlbvh/refit.hlsl          -Fo hlbvh/refit.bin
+ dxc %FLAGS% radix/histogram.hlsl      -Fo radix/histogram.bin
+ dxc %FLAGS% radix/permute.hlsl        -Fo radix/permute.bin
+ dxc %FLAGS% radix/prefix-scan.hlsl    -Fo radix/prefix-scan.bin
 pause
--- directx-sdk/render/intersection.hlsl
+++ directx-sdk/render/intersection.hlsl
@@ -1,15 +1,11 @@
 
-#include "./include/constants.hlsl"
-#include "./include/structs.hlsl"
-#include "./include/uniforms.hlsl"
-#include "./include/STOmath.hlsl"
-#include "./include/vertex.hlsl"
-
-
-
-
-
-#include "./hlbvh/traverse.hlsl"
+#include "../include/constants.hlsl"
+#include "../include/structs.hlsl"
+#include "../include/uniforms.hlsl"
+#include "../include/STOmath.hlsl"
+#include "../include/vertex.hlsl"
+#include "../include/rays.hlsl"
+#include "../hlbvh/traverse.hlsl"
 
 [numthreads(WORK_SIZE, 1, 1)]
 void main( uint3 WorkGroupID : SV_DispatchThreadID, uint3 LocalInvocationID : SV_GroupID, uint3 GlobalInvocationID : SV_GroupThreadID, uint LocalInvocationIndex : SV_GroupIndex )
