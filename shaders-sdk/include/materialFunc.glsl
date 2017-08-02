eb32e4f84e9d4a0fa879c165718a07b043746e6b
CapitalKnew <capitalknew@users.noreply.github.com>
2017-08-02 16:42:12 +0800

    bicubic interpolation

--- shaders-sdk/include/materialFunc.glsl
+++ shaders-sdk/include/materialFunc.glsl
@@ -50,10 +50,57 @@ layout ( binding = 6, set = 1 ) uniform sampler2D samplers[MAX_TEXTURES]; // vul
 #endif
 
 
+
+vec4 cubic(in float v){
+    vec4 n = vec4(1.0, 2.0, 3.0, 4.0) - v;
+    vec4 s = n * n * n;
+    float x = s.x;
+    float y = s.y - 4.0 * s.x;
+    float z = s.z - 4.0 * s.y + 6.0 * s.x;
+    float w = 6.0 - x - y - z;
+    return vec4(x, y, z, w) * (1.0/6.0);
+}
+
+vec4 textureBicubic(in sampler2D sampler, in vec2 texCoords){
+    vec2 texSize = textureSize(sampler, 0);
+    vec2 invTexSize = 1.0 / texSize;
+    texCoords = texCoords * texSize - 0.5;
+
+    vec2 fxy = fract(texCoords);
+    texCoords -= fxy;
+
+    vec4 xcubic = cubic(fxy.x);
+    vec4 ycubic = cubic(fxy.y);
+
+    vec4 c = texCoords.xxyy + vec2 (-0.5, +1.5).xyxy;
+    vec4 s = vec4(xcubic.xz + xcubic.yw, ycubic.xz + ycubic.yw);
+    vec4 offset = c + vec4 (xcubic.yw, ycubic.yw) / s;
+
+    offset *= invTexSize.xxyy;
+
+    vec4 sample0 = texture(sampler, offset.xz);
+    vec4 sample1 = texture(sampler, offset.yz);
+    vec4 sample2 = texture(sampler, offset.xw);
+    vec4 sample3 = texture(sampler, offset.yw);
+
+    float sx = s.x / (s.x + s.y);
+    float sy = s.z / (s.z + s.w);
+
+    return mix(
+       mix(sample3, sample2, sx), mix(sample1, sample0, sx)
+    , sy);
+}
+
+
+
 vec4 readEnv(in vec3 r) {
     vec3 nr = normalize(r);
-    return texture(skybox[0], vec2(fma(vec2(atan(nr.z, nr.x), asin(nr.y) * 2.0f) / PI, vec2(0.5), vec2(0.5))));
+    return textureBicubic(skybox[0], vec2(fma(vec2(atan(nr.z, nr.x), asin(nr.y) * 2.0f) / PI, vec2(0.5), vec2(0.5))));
 }
+
+
+
+
 
 
 
@@ -70,11 +117,11 @@ bool validateTexture(in uint binding){
 }
 
 vec4 fetchPart(in uint binding, in vec2 texcoord){
-    return texture(samplers[binding], texcoord);
+    return textureBicubic(samplers[binding], texcoord);
 }
 
 vec4 fetchPart(in uint binding, in vec2 texcoord, in ivec2 offset){
-    return texture(samplers[binding], texcoord + vec2(offset) / textureSize(samplers[binding], 0));
+    return textureBicubic(samplers[binding], texcoord + vec2(offset) / textureSize(samplers[binding], 0));
 }
 
 vec4 fetchSpecular(in Submat mat, in vec2 texcoord, in vec3 direct, in vec3 normal){
