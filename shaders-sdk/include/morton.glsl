3bc03eb63048549e85111980a62ab54e2407688e
CapitalKnew <capitalknew@users.noreply.github.com>
2017-08-10 07:17:40 +0800

    use magic numbers

--- shaders-sdk/include/morton.glsl
+++ shaders-sdk/include/morton.glsl
@@ -30,18 +30,13 @@ uint64_t MortonToHilbert3D( const uint64_t morton, const uint64_t bits ) {
 }
 
 uint64_t part1By2_64(in uint a){
-    /*
-    uint64_t x = a & 0x1ffffful; // we only look at the first 21 bits
-    x = (x | x << 32) & 0x1f00000000fffful;  // shift left 32 bits, OR with self, and 00011111000000000000000000000000000000001111111111111111
-    x = (x | x << 16) & 0x1f0000ff0000fful;  // shift left 32 bits, OR with self, and 00011111000000000000000011111111000000000000000011111111
-    x = (x | x << 8) & 0x100f00f00f00f00ful; // shift left 32 bits, OR with self, and 0001000000001111000000001111000000001111000000001111000000000000
-    x = (x | x << 4) & 0x10c30c30c30c30c3ul; // shift left 32 bits, OR with self, and 0001000011000011000011000011000011000011000011000011000100000000
+    uint64_t x = a & 0x1ffffful;
+    x = (x | x << 32) & 0x1f00000000fffful;
+    x = (x | x << 16) & 0x1f0000ff0000fful;
+    x = (x | x << 8) & 0x100f00f00f00f00ful;
+    x = (x | x << 4) & 0x10c30c30c30c30c3ul;
     x = (x | x << 2) & 0x1249249249249249ul;
     return x;
-    */
-    uint64_t answer = 0;
-    for (uint64_t i = 0; i < 21; ++i) { answer |= ((uint64_t(a) & (1ul << i)) << 2*i); }
-    return answer;
 }
 
 uint64_t encodeMorton3_64(in uvec3 a)
