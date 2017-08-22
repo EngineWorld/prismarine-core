32ea9d3acfe87daf28b2380f66316c6ac0392ebb
CapitalKnew <capitalknew@users.noreply.github.com>
2017-08-22 12:50:24 +0800

    optimize read-write overhead

--- Include/Prismarine/Radix.hpp
+++ Include/Prismarine/Radix.hpp
@@ -33,20 +33,23 @@ namespace ppr {
         }
 
         void sort(GLuint &InKeys, GLuint &InVals, uint32_t size = 1, uint32_t descending = 0) {
-            glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 20, InKeys);
-            glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 21, InVals);
-            glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 22, OutKeys);
-            glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 23, OutValues);
+            bool swapness = true;
             glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 24, VarBuffer);
 
-            //for (GLuint i = 0; i < 16; i++) { // 64-bit uint
             for (GLuint i = 0; i < 8; i++) { // 64-bit uint
+                glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 20, swapness ? InKeys : OutKeys);
+                glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 21, swapness ? InVals : OutValues);
+                glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 22, swapness ? OutKeys : InKeys);
+                glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 23, swapness ? OutValues : InVals);
+
                 Consts consts = { size, i, descending, 0 };
                 glNamedBufferSubData(VarBuffer, 0, strided<Consts>(1), &consts);
                 dispatch(sortProgram, 1);
-                glCopyNamedBufferSubData(OutKeys, InKeys, 0, 0, strided<uint64_t>(size));
-                glCopyNamedBufferSubData(OutValues, InVals, 0, 0, strided<uint32_t>(size));
+                swapness = !swapness;
             }
+
+            if (!swapness) glCopyNamedBufferSubData(OutKeys, InKeys, 0, 0, strided<uint64_t>(size));
+            if (!swapness) glCopyNamedBufferSubData(OutValues, InVals, 0, 0, strided<uint32_t>(size));
         }
 
     };
