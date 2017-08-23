72f810a2575866ebea921c357e9896ad6dc5edca
CapitalKnew <capitalknew@users.noreply.github.com>
2017-08-23 20:24:59 +0800

    more WARP optimized, fixed libs

--- ShadersSDK/include/ballotlib.glsl
+++ ShadersSDK/include/ballotlib.glsl
@@ -107,45 +107,6 @@ int firstActive(){
     return (lv >= 0 ? lv : (32+findLSB(bits.y)));
 }
 
-
-
-
-
-// bit logic
-const int bx = 1, by = 2, bz = 4, bw = 8;
-const int bswiz[8] = {1, 2, 4, 8, 16, 32, 64, 128};
-
-int cB(in bool a, in int swizzle){
-    return a ? swizzle : 0;
-}
-
-
-int cB4(in bvec4 a){
-    ivec4 mx4 = mix(ivec4(0), ivec4(bx, by, bz, bw), a);
-    ivec2 mx2 = mx4.xy | mx4.wz; // swizzle or
-    return (mx2.x | mx2.y); // rest of
-}
-
-int cB2(in bvec2 a){
-    ivec2 mx = mix(ivec2(0), ivec2(bx, by), a);
-    return (mx.x | mx.y);
-}
-
-bvec2 cI2(in int a){
-    return bvec2(
-        (a & bx) > 0, 
-        (a & by) > 0
-    );
-}
-
-bool anyB(in int a){
-    return a > 0;
-}
-
-bool allB2(in int a){
-    return a == 3;
-}
-
 #endif
 
 #define initAtomicIncFunction(mem, fname, T)\
