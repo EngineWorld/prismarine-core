ef9b641f256a447381da3f5128ec2b294516d8ef
CapitalKnew <capitalknew@users.noreply.github.com>
2017-09-16 05:58:03 +0800

    allocate

--- Source/Examples/Viewer.cpp
+++ Source/Examples/Viewer.cpp
@@ -229,8 +229,8 @@ namespace PrismarineExample {
 
 		// create geometry intersector
 		intersector = new psm::TriangleHierarchy();
-		intersector->allocate(1024 * 512);
-		//intersector->allocate(1024 * 2048);
+		//intersector->allocate(1024 * 512);
+		intersector->allocate(1024 * 2048);
 
 		// init timing state
 		time = glfwGetTime() * 1000.f;
